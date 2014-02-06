9b80d2c5e71758c4b0ab827288b8fffde2fd12f0
Daydream Qian <daydream.qian@gmail.com>
2014-02-07 00:59:49 +0800

    建立ThinkPHP框架

---
+++ APP/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+	'APP_GROUP_LIST' => 'Index,Admin' , 
+	'DEFAULT_GROUP' => 'Index' , 
+	'TMPL_PARSE_STRING' => array (
+		'__PUBLIC__' => __ROOT__ .'/'.'Greamer'.'/Public',	
+	) , 
+);
+?>
---
+++ APP/Lib/Action/Index/IndexAction.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 前端首页
+ */
+
+class IndexAction extends Action {
+
+	public function index () {
+
+		$this -> display () ; 
+
+
+	}
+	
+	
+}
+
+
+?>
